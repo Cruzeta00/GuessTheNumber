e9e288a4bb6a348ad5dc920820481f42390a845a
Júlio César <julio10as@gmail.com>
2021-07-13 17:52:59 -0300

    Contemplada issue#5 nessa solução.

--- GuessTheNumber/JogoIniciado/JogoRodando.cs
+++ GuessTheNumber/JogoIniciado/JogoRodando.cs
@@ -17,28 +17,30 @@ namespace GuessTheNumber.JogoIniciado
             int tempoDificuldade = Tempos.RetornarTempo(dificuldadeSelecionada);
             int numeroChutado;
             tempoCorrido.Start();
-            while (!int.TryParse(Console.ReadLine(), out numeroChutado))
-            {
-                Console.WriteLine("Valor Inválido.");
-            }
+            Console.WriteLine("VALENDO!!");
             while (tempoCorrido.Elapsed.TotalSeconds <= tempoDificuldade)
             {
-                if (numeroChutado == numeroGerado)
+                int novoChute;
+                bool eUmNumero = int.TryParse(Console.ReadLine(), out novoChute);
+                if (eUmNumero)
                 {
-                    tempoCorrido.Stop();
-                    Console.WriteLine($"PARABÉNS!!! Você acertou o meu número pensado em {tempoCorrido.Elapsed.TotalSeconds} segundos. Será que da para bater " +
-                        $"o seu recorde?");
-                    Console.WriteLine("Gostaria de jogar novamente? 0 - SIM, 1 - NÃO");
-                    Console.WriteLine("0 - SIM");
-                    Console.WriteLine("1 - NÃO");
-                    int.TryParse(Console.ReadLine(), out int jogarNovamenteVitoria);
-                    if (jogarNovamenteVitoria == 0) Jogo.InicializarJogo();
-                    if (jogarNovamenteVitoria == 1) return;
+                    numeroChutado = novoChute;
+                    if (numeroChutado == numeroGerado)
+                    {
+                        tempoCorrido.Stop();
+                        Console.WriteLine($"PARABÉNS!!! Você acertou o meu número pensado em {tempoCorrido.Elapsed.TotalSeconds} segundos. Será que da para bater " +
+                            $"o seu tempo?");
+                        Console.WriteLine("Gostaria de jogar novamente? 0 - SIM, 1 - NÃO");
+                        Console.WriteLine("0 - SIM");
+                        Console.WriteLine("1 - NÃO");
+                        int.TryParse(Console.ReadLine(), out int jogarNovamenteVitoria);
+                        if (jogarNovamenteVitoria == 0) Jogo.InicializarJogo();
+                        if (jogarNovamenteVitoria == 1) return;
+                    }
+                    else if (numeroChutado < numeroGerado) Console.WriteLine("Muito baixo");
+                    else if (numeroChutado > numeroGerado) Console.WriteLine("Muito Alto");
                 }
-                else if (numeroChutado < numeroGerado) Console.WriteLine("Muito baixo");
-                else if (numeroChutado > numeroGerado) Console.WriteLine("Muito Alto");
-                int.TryParse(Console.ReadLine(), out int novoChute);
-                numeroChutado = novoChute;
+                else Console.WriteLine("Então... Você precisa acertar um número. Que tal tentar com valores númericos dessa vez?");
             }
             Console.WriteLine($"Tempo esgotado. Não foi dessa vez, amigo. O número gerado era o {numeroGerado}.");
             Console.WriteLine("Gostaria de jogar novamente?");
